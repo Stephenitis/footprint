ae8faae98128d163928992f44bd49b631b1e195d
Mitch Seaman <mitchel.seaman@gmail.com>
2013-08-10 15:29:28 -0700

    styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,35 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-user = User.new(name:"user",
+user = User.new(name:"Stephen",
 								email:"nguyenhstephen@gmail.com",
 								zipcode:94117)
 user.password = "password"
 user.save
 
-user = User.new(name:"mitch",
+user = User.new(name:"Mitch",
 								email:"mitchel.seaman@gmail.com",
+								zipcode:94117)
+user.password = "password"
+# user.moves_access_token = 'mfqJ5ApA51SA1BXV1Tr7G9B_6oPTdYyMllb63U9kOq_hy33pQkFnib_G1bL7H45E'
+user.save
+
+user = User.new(name:"Eric",
+								email:"eric.chen0121@gmail.com",
+								zipcode:94117)
+user.password = "password"
+# user.moves_access_token = 'mfqJ5ApA51SA1BXV1Tr7G9B_6oPTdYyMllb63U9kOq_hy33pQkFnib_G1bL7H45E'
+user.save
+
+user = User.new(name:"Daniel",
+								email:"dmkwillems@gmail.com",
+								zipcode:94117)
+user.password = "password"
+# user.moves_access_token = 'mfqJ5ApA51SA1BXV1Tr7G9B_6oPTdYyMllb63U9kOq_hy33pQkFnib_G1bL7H45E'
+user.save
+
+user = User.new(name:"Jared",
+								email:"jaredsmithse@icloud.com",
 								zipcode:94117)
 user.password = "password"
 # user.moves_access_token = 'mfqJ5ApA51SA1BXV1Tr7G9B_6oPTdYyMllb63U9kOq_hy33pQkFnib_G1bL7H45E'
