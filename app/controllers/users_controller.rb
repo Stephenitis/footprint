cc35f53a72e04d4a43b41416d7840e354199c799
Eric Savvy <eric.chen0121@gmail.com>
2013-08-10 15:10:31 -0700

    added bg images for user score

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,7 +46,7 @@ class UsersController < ApplicationController
       daily_CO2_footprint = berkeley_calc(daily_trip)
 
       # writing event to db
-      @user.events << Event.create(meters_driven: daily_trip,  score: 90, meters_walked: daily_walk, meters_biked: daily_cycle, carbon_footprint: daily_CO2_footprint)
+      @user.events << Event.create(meters_driven: daily_trip, meters_walked: daily_walk, meters_biked: daily_cycle, carbon_footprint: daily_CO2_footprint)
       @daily_events = @user.events.last
     end
   end
