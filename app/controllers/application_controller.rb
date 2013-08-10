ab96c8adc7b1ebbcb22bc07462aae458c1ee4b36
michaelchihuyho <michaelchihuyho@gmail.com>
2013-08-10 01:05:31 -0700

    Added login/session helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def current_user=(user)
+    session[:user_id] = user.id
+  end
+  
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  helper_method :current_user, :logged_in?
 end
