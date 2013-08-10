6cf7bd197d86dee286202e14782eaccabbd76d37
Eric Savvy <eric.chen0121@gmail.com>
2013-08-09 23:59:54 -0700

    ADDING RAILS FUCKING 3.2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Footprint::Application.config.secret_token = 'f040fed58a644d7ed5fc44f8795dffd213c916ddd2dc5cf9e717f4953c67f3e804bb531476f3511cc3d383824d5e7a6fd3dfbe439851148bbbff70829d720f84'
