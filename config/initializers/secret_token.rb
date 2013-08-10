dd5a5eb533598095be202e87f7e37e459751e7d7
michaelchihuyho <michaelchihuyho@gmail.com>
2013-08-09 23:26:03 -0700

    Added barebones rails 3.2.13 app without testing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Footprint::Application.config.secret_key_base = '6b0f521fa843d129ab345f904c98455734d49e25c2aedcbee463cf24cea24e822a66ec4d8b0bd37726e8db36a3fb2cd12c8574bb519294e4fa47ca64b8391a41'
