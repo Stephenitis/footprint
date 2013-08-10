da1de4aac2c7b750b889f9ab92136dca9cc29b36
Jared Smith <jared.rit@me.com>
2013-08-10 11:43:38 -0700

    Add self referential relationship for friendships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
+	attr_accessible :name, :email, :zipcode, :score, :password, :password_confirmation, :password_digest
+
 	validates :name, :email, :zipcode, presence: true
 	validates :zipcode, length: {is: 5}
+
+	has_many :friendships
+	has_many :friends, through: :friendships
 	has_many :events
   has_secure_password
 end
